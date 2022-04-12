0296f786013ccc665e6b2583f1528627cf6af10a
Victor Scherer Putrich <scherer.victor98@gmail.com>
2022-04-12 18:47:37 -0300

    Minimax warning fixes

--- src/agents/minimax.py
+++ src/agents/minimax.py
@@ -11,10 +11,10 @@ class MinimaxAgent(Agent):
         else:
             return 0
     
-    def minimax(self, context, depth, maximizingMoveer):
+    def minimax(self, context, depth, maximizing_move):
         if depth == 0 or self.game.is_terminal_state(context):
             return self.get_heuristc_value(context), None
-        if maximizingMoveer:
+        if maximizing_move:
             max_value = -Inf
             max_move = None
             for legal_move in self.game.moves(context, self.player):
@@ -24,16 +24,16 @@ class MinimaxAgent(Agent):
                     max_value = new_value
                     max_move = legal_move
             return max_value, max_move
-        else:
-            min_value = Inf
-            min_move = None
-            for legal_move in self.game.moves(context, self.get_opponent()):
-                new_context = self.game.apply(legal_move, context)
-                new_value, move = self.minimax(new_context, depth - 1, True)
-                if new_value < min_value:
-                    min_value = new_value
-                    min_move = legal_move
-            return min_value, min_move
+        
+        min_value = Inf
+        min_move = None
+        for legal_move in self.game.moves(context, self.get_opponent()):
+            new_context = self.game.apply(legal_move, context)
+            new_value, move = self.minimax(new_context, depth - 1, True)
+            if new_value < min_value:
+                min_value = new_value
+                min_move = legal_move
+        return min_value, min_move
 
     def select_action(self, game, context, max_second=0, max_iterations=0, max_depth=4):
         value, move = self.minimax(context, max_depth, True)
